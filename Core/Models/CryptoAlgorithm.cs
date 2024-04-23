048c44fb273f22f4dfcbc776d90fe78d1b071e68
Reese Pollard <rpollard@gmail.com>
2024-04-23 15:13:13 -0700

    Required Foreign Keys for One to Many

--- Core/Models/CryptoAlgorithm.cs
+++ Core/Models/CryptoAlgorithm.cs
@@ -3,5 +3,6 @@ namespace Core.Models;
 public class CryptoAlgorithm
 {
     public long Id { get; set; }
+    public long CertificateId { get; set; }
     public string Name { get; set; } = String.Empty;
 }
--- Core/Models/Issuer.cs
+++ Core/Models/Issuer.cs
@@ -3,5 +3,6 @@ namespace Core.Models;
 public class Issuer
 {
     public long Id { get; set; }
+    public long CertificateId { get; set; }
     public string? Name { get; set; }
 }
