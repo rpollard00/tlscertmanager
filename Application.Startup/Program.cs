47fc1e9e2d7d2d9c389719d1c2c424cb2c1bc268
Reese Pollard <rpollard@gmail.com>
2024-04-24 22:50:15 -0700

    Configure Middleware

--- Application.Startup/Program.cs
+++ Application.Startup/Program.cs
@@ -1,7 +1,15 @@
-﻿var api = new AdapterApi(args, options =>
+﻿using Adapter.Api.SQLite.DataAccess;
+using Adapter.Api.SQLite;
+using Adapter.Api.Adapter;
+using Microsoft.Extensions.DependencyInjection;
+var api = new AdapterApi(args, options =>
 {
-
-    // work to be done
+    options.AddScoped<ICertDbContext, CertDbContext>();
+    options.AddScoped<ICertificateCreator, CertificateDataAdapterCreate>();
+    options.AddScoped<ICertificateRetriever, CertificateDataAdapterRetrieve>();
+    options.AddScoped<ICertificateRemover, CertificateDataAdapterRemove>();
+    options.AddScoped<ICertificateUpdater, CertificateDataAdapterUpdate>();
+    options.AddScoped<ICertificateService, ApiAdapter>();
 });
 
 await api.RunAsync();
