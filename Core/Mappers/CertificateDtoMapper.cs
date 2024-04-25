f2aad85962d630a6f1969c42207eb87a77d8efbc
Reese Pollard <rpollard@gmail.com>
2024-04-24 23:39:37 -0700

    Cert to Dto Mapper

---
+++ Core/Mappers/CertificateDtoMapper.cs
@@ -0,0 +1,57 @@
+using Core.Models;
+using Core.Dtos;
+
+namespace Core.Mapper;
+
+public static class CertificateDtoMapper
+{
+    public static CertificateDto? CertificateToDto(Certificate cert)
+    {
+
+        CertificateDto certDto = new()
+        {
+            Id = cert.Id,
+            SubjectName = cert.SubjectName,
+
+            IssueDate = cert.IssueDate,
+            ExpirationDate = cert.ExpirationDate,
+
+            Issuer = cert.Issuer.ToString() ?? "",
+            CryptoAlgorithm = cert.CryptoAlgorithm.ToString() ?? "",
+
+            SubjectAlternateNames = cert.SubjectAlternateNames
+                                    != null ? cert.SubjectAlternateNames
+                                    .Select(s => s.ToString() ?? "")
+                                    .ToList() : null,
+
+            SystemNode = cert.SystemNode
+                         != null ? cert.SystemNode
+                         .Select(s => s.ToString() ?? "")
+                         .ToList() : null,
+
+            // TODO: Not hardcode the threshold
+            isExpiring = cert.isExpiring(30),
+
+        };
+
+        return certDto;
+    }
+}
+
+// public class CertificateDto
+// {
+//     public long Id { get; set; }
+//     public string? SubjectName { get; set; }
+//
+//     public long IssueDate { get; set; }
+//     public long ExpirationDate { get; set; }
+//
+//     public required string CryptoAlgorithm { get; set; }
+//     public required string Issuer { get; set; }
+//
+//     public List<string> SubjectAlternateNames { get; set; } = new();
+//     public List<string> SystemNode { get; set; } = new();
+//
+//     public bool isExpiring { get; set; }
+//
+// }
