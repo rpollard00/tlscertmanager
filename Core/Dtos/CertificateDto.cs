61a6af4293aead85d1a7fd26cc002aa99650af27
Reese Pollard <rpollard@gmail.com>
2024-04-24 23:01:51 -0700

    Cert Dto

---
+++ Core/Dtos/CertificateDto.cs
@@ -0,0 +1,20 @@
+namespace Core.Dtos;
+
+public class CertificateDto
+{
+    public long Id { get; set; }
+    public string? SubjectName { get; set; }
+
+    public long IssueDate { get; set; }
+    public long ExpirationDate { get; set; }
+
+    public required string CryptoAlgorithm { get; set; }
+    public required string Issuer { get; set; }
+
+    public List<string> SubjectAlternateNames { get; set; } = new();
+    public List<string> SystemNode { get; set; } = new();
+
+    public bool isExpiring { get; set; }
+
+}
+
