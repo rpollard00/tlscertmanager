4a42abd18b31d161f8ca28f360a62929ac86dc71
Reese Pollard <rpollard@gmail.com>
2024-05-01 21:22:00 -0700

    Dev API Addr

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -7,7 +7,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5017/"),
+});
 builder.Services.AddSingleton<StateContainer>();
 
 await builder.Build().RunAsync();
